7aba5a5387e85efb738aed6a16e6904b27b36bfc
Margoth <mloreana@gmail.com>
2021-02-14 03:00:15 +0000

    Excercise about generating an excuse using javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+function excusegenerator(){
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+
+let message=
+who[Math.floor(Math.random() * who.length)] + " " +
+what[Math.floor(Math.random() * what.length)] + " " +
+when[Math.floor(Math.random() * when.length)];
+
+document.getElementById("excuse").innerHTML = message;
+}
